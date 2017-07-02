73936c5f5f9912fc710f6bada29359400291f320
moKa craft <mokacraft@gmail.com>
2017-07-02 16:05:56 +0200

    streaming

--- src/controller/EventlogController.java
+++ src/controller/EventlogController.java
@@ -5,16 +5,35 @@
 */
 package controller;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import static controller.Main.socket;
+import io.socket.client.IO;
+import io.socket.emitter.Emitter;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.SocketException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -25,8 +44,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.imageio.ImageIO;
+import org.apache.commons.codec.binary.Base64;
 
 import utils.DialogSys;
+import utils.security.encryption;
 /**
  * FXML Controller class
  *
@@ -51,56 +76,40 @@ public class EventlogController extends Controller implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        new Thread(new Runnable() {
+        
+    }
+    
+    @FXML
+    public void action1(ActionEvent event) throws IOException, URISyntaxException {
+        socket = IO.socket("http://83.113.70.210:8085");
+        socket.on("test", new Emitter.Listener() {
             @Override
-            public void run() {
-                DatagramSocket serverSocket = null;
-                try {
-                    serverSocket = new DatagramSocket(4000);
-                } catch (SocketException ex) {
-                    Logger.getLogger(EventlogController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                    byte[] receiveData = new byte[65000];
-                    byte[] sendData = new byte[65000];
-                    while(true)
-                       {
-                        try {
-                            System.out.println("here");
-                            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                            serverSocket.receive(receivePacket);
-                            String sentence = new String( receivePacket.getData());
-                            System.out.println("RECEIVED: " + sentence.length());
-                            System.out.println("RECEIVED: " + sentence.charAt(62010));
-                            
-//                            InetAddress IPAddress = receivePacket.getAddress();
-//                            int port = receivePacket.getPort();
-//                            String capitalizedSentence = sentence.toUpperCase();
-//                            sendData = capitalizedSentence.getBytes();
-//                            DatagramPacket sendPacket =
-//                                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
-//                            serverSocket.send(sendPacket);
-                        } catch (IOException ex) {
-                            Logger.getLogger(EventlogController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                       }
-                }
-        }).start();
+            public void call(Object... args) {
+                System.out.println("test");
+            }
+        });
+        socket.connect();
     }
     
     @FXML
-    public void action1(ActionEvent event) throws IOException {
-        DatagramSocket ds = null;
-        ds = new DatagramSocket();
-        byte[] mybytearray = new byte[63000];
-        mybytearray[62010] = 'a';
-        System.out.println(mybytearray.length);
-        DatagramPacket dp = new DatagramPacket(mybytearray, mybytearray.length, InetAddress.getByName("83.113.70.210"), 4000);
-        ds.send(dp);
-    }
-    
-    @FXML
-    public void action2(ActionEvent event) throws IOException, NoSuchAlgorithmException {
+    public void action2(ActionEvent event) throws IOException, NoSuchAlgorithmException, AWTException {
+        Rectangle rec=new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        Robot r= new Robot();
+        BufferedImage img;
+        while (1 == 1) {
+        img = r.createScreenCapture(rec);
 
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        if(ImageIO.write(img, "png",baos))
+        {
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            int i = 0;
+            System.out.println(i++ + "length " + imageInByte.length);
+            baos.close();
+            socket.emit("image", Base64.encodeBase64String(imageInByte));
+        }
+       }
     }
 }
 
