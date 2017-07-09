4046e73fed187393aa185dde16c599fdf869e9c4
moKacraft <mokacraft@gmail.com>
2017-07-09 20:07:31 +0200

    give up cleaning

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -26,81 +26,73 @@ import service.ServiceFactory;
 
 public class Main extends Application
 {
-	public static Stage parentWindow;
-        public static Socket socket;
-        public static JDialog dialogSend;
-        public static JLabel labelSend;
+    public static Stage parentWindow;
+    public static Socket socket;
+    public static JDialog dialogSend;
+    public static JLabel labelSend;
 	
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args)
-	{
-            dialogSend = new JDialog();
-            labelSend = new JLabel("Please wait...");
-            dialogSend.setLocationRelativeTo(null);
-            dialogSend.setTitle("Please Wait...");
-            dialogSend.add(labelSend);
-            dialogSend.setAlwaysOnTop(true);
-            dialogSend.setPreferredSize(new Dimension(300,100));
-            dialogSend.pack();
-            Application.launch(Main.class, (java.lang.String[]) null);
-                
-	}
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args)
+    {
+        dialogSend = new JDialog();
+        labelSend = new JLabel("Please wait...");
+        dialogSend.setLocationRelativeTo(null);
+        dialogSend.setTitle("Please Wait...");
+        dialogSend.add(labelSend);
+        dialogSend.setAlwaysOnTop(true);
+        dialogSend.setPreferredSize(new Dimension(300,100));
+        dialogSend.pack();
+        Application.launch(Main.class, (java.lang.String[]) null);         
+    }
 	
-	public static void loadScene(String sceneName, String title) throws IOException
-	{
-		ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
-		ResourceBundle bundle = ResourceBundle.getBundle("lang.lang", new Locale(configManager.getConfig().getLang()));
-		
-		AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource(sceneName), bundle);
-		parentWindow.setTitle("Slidare - " + bundle.getString(title));
-		parentWindow.getScene().setRoot(page); 
-	}
+    public static void loadScene(String sceneName, String title) throws IOException
+    {
+        ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
+        ResourceBundle bundle = ResourceBundle.getBundle("lang.lang", new Locale(configManager.getConfig().getLang()));
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
-		try
-		{
-			ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
-			configManager.loadBase();
+        AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource(sceneName), bundle);
+        parentWindow.setTitle("Slidare - " + bundle.getString(title));
+        parentWindow.getScene().setRoot(page); 
+    }
+	
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        try {
+            ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
+            configManager.loadBase();
 			
-			ConnectController connectctrl = new ConnectController();
+            ConnectController connectctrl = new ConnectController();
+            parentWindow = primaryStage;
+            ResourceBundle bundle = ServiceFactory.getResourceBundle(false);
+            AnchorPane page;
+            connectctrl.checkIdentification("flavien.maillot@epitech.eu", "admin");
 			
-			parentWindow = primaryStage;
-			
-			ResourceBundle bundle = ServiceFactory.getResourceBundle(false);
-			AnchorPane page;
-			
-			connectctrl.checkIdentification("flavien.maillot@epitech.eu", "admin");
-			
-			if (configManager.getConfig().getAutoConnect()== true && connectctrl.checkIdentification(configManager.getConfig().getUsername(), configManager.getConfig().getPassword()))
-                        {
-                            page = (AnchorPane) FXMLLoader.load(Main.class.getResource("/view/ContactTracking.fxml"), bundle);
-                            try {
-                                DragDropTracking dragDropTracking = new DragDropTracking();
-                            } catch (NativeHookException ex) {
-                            Logger.getLogger(ConnectController.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        }
-                        else {
-                            page = (AnchorPane) FXMLLoader.load(Main.class.getResource("/view/connect.fxml"), bundle);
-                            try {
-                                DragDropTracking dragDropTracking = new DragDropTracking();
-                            } catch (NativeHookException ex) {
-                                Logger.getLogger(ConnectController.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        }
-			Scene scene = new Scene(page);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Slidare - " + bundle.getString("Connect_title"));
-			configManager.configStage(primaryStage);
-			primaryStage.show();	
-		}
-		catch (Exception ex)
-		{
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-		}
+            if (configManager.getConfig().getAutoConnect()== true && connectctrl.checkIdentification(configManager.getConfig().getUsername(), configManager.getConfig().getPassword()))
+            {
+                page = (AnchorPane) FXMLLoader.load(Main.class.getResource("/view/ContactTracking.fxml"), bundle);
+                try {
+                    DragDropTracking dragDropTracking = new DragDropTracking();
+                } catch (NativeHookException ex) {
+                    Logger.getLogger(ConnectController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                page = (AnchorPane) FXMLLoader.load(Main.class.getResource("/view/connect.fxml"), bundle);
+                try {
+                    DragDropTracking dragDropTracking = new DragDropTracking();
+                } catch (NativeHookException ex) {
+                    Logger.getLogger(ConnectController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Slidare - " + bundle.getString("Connect_title"));
+            configManager.configStage(primaryStage);
+            primaryStage.show();	
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 	}
+    }
 }
--- src/controller/MenubarController.java
+++ src/controller/MenubarController.java
@@ -40,141 +40,135 @@ import utils.streaming.Settings;
 public class MenubarController implements Initializable
 {
 
-	@FXML
-	private HBox MenuBarHB;
+    @FXML
+    private HBox MenuBarHB;
         
-        private static Frame grabbedFrame;
+    private static Frame grabbedFrame;
 	
-	@Override
-	public void initialize(URL location, ResourceBundle resources)
-	{
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+    }
+
+    public void loadConfig(ActionEvent event) throws IOException
+    {
+        FileManager fileManager = (FileManager) ServiceFactory.getFileManager();
+	fileManager.initFileChooserConfig(Main.parentWindow);
+	String filepath = fileManager.getFilePath();
+
+	if (!filepath.equals("")) {
+            ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
+            configManager.load(filepath);
+            configManager.configStage(Main.parentWindow);
+            Main.loadScene("/view/ContactTracking.fxml", "Contact_title");
 	}
+    }
 
-	public void loadConfig(ActionEvent event) throws IOException
-	{
-		FileManager fileManager = (FileManager) ServiceFactory.getFileManager();
-		fileManager.initFileChooserConfig(Main.parentWindow);
-		String filepath = fileManager.getFilePath();
-
-		if (!filepath.equals("")) {
-			ConfigManager configManager = (ConfigManager) ServiceFactory.getConfigManager();
-			configManager.load(filepath);
-			configManager.configStage(Main.parentWindow);
-			Main.loadScene("/view/ContactTracking.fxml", "Contact_title");
-		}
-	}
-
-	public void logout(ActionEvent event) throws IOException
-	{
-		ConfigManager cfg = (ConfigManager) ServiceFactory.getConfigManager();
-		cfg.getConfig().setUsername("");
-		cfg.getConfig().setPassword("");
-		cfg.getConfig().setAutoConnect(false);
-		cfg.save();
+    public void logout(ActionEvent event) throws IOException
+    {
+        ConfigManager cfg = (ConfigManager) ServiceFactory.getConfigManager();
+	cfg.getConfig().setUsername("");
+	cfg.getConfig().setPassword("");
+	cfg.getConfig().setAutoConnect(false);
+	cfg.save();
 		
-		//Chargement de la page de connection
-		Main.loadScene("/view/connect.fxml", "Connect_title");
-	}
+	//Chargement de la page de connection
+	Main.loadScene("/view/connect.fxml", "Connect_title");
+    }
 	
-	public void quitFired(ActionEvent event)
-	{
-		Platform.exit();
-		System.exit(0);
-	}
+    public void quitFired(ActionEvent event)
+    {
+        Platform.exit();
+	System.exit(0);
+    }
 
-	public void switchScene(ActionEvent event) throws IOException
-	{
-		MenuItem menu = (MenuItem) event.getSource();
-		String menuId = menu.getId();
-		if (null != menuId) {
-			switch (menuId)
-			{
-				case "manageaccount":
-					Main.loadScene("/view/AccountTracking.fxml", "Account_title");
-					break;
-				case "managegroups":
-					Main.loadScene("/view/GroupTracking.fxml", "Group_title");
-					break;
-				case "managecontacts":
-					Main.loadScene("/view/ContactTracking.fxml", "Contact_title");
-					break;
-				case "setting":
-					Main.loadScene("/view/ConfigTracking.fxml", "Contact_title");
-					break;
-                                case "managedragdrop":
-					Main.loadScene("/view/DragDropTracking.fxml", "DragDrop_title");
-					break;
-				case "eventlog":
-//					DialogSys ds = new DialogSys();
-//					ds.showPopup(MenuBarHB, "/view/eventlog.fxml", "Eventlog");
-                                        utils.streaming.Controller controller = new utils.streaming.Controller();
-                                        Settings settings = controller.getSettings();
+    public void switchScene(ActionEvent event) throws IOException
+    {
+        MenuItem menu = (MenuItem) event.getSource();
+	String menuId = menu.getId();
+	if (null != menuId) {
+            switch (menuId) {
+		case "manageaccount":
+                    Main.loadScene("/view/AccountTracking.fxml", "Account_title");
+                    break;
+		case "managegroups":
+                    Main.loadScene("/view/GroupTracking.fxml", "Group_title");
+                    break;
+		case "managecontacts":
+                    Main.loadScene("/view/ContactTracking.fxml", "Contact_title");
+                    break;
+                case "setting":
+                    Main.loadScene("/view/ConfigTracking.fxml", "Contact_title");
+                    break;
+                case "managedragdrop":
+                    Main.loadScene("/view/DragDropTracking.fxml", "DragDrop_title");
+                    break;
+		case "eventlog":
+                    utils.streaming.Controller controller = new utils.streaming.Controller();
+                    Settings settings = controller.getSettings();
+                    Toolkit kit = Toolkit.getDefaultToolkit();
+                    Dimension screenSize = kit.getScreenSize();
+                    int screenWidth = screenSize.width;
+                    int screenHeight = screenSize.height;
 
-
-                                        Toolkit kit = Toolkit.getDefaultToolkit();
-                                        Dimension screenSize = kit.getScreenSize();
-                                        int screenWidth = screenSize.width;
-                                        int screenHeight = screenSize.height;
-
-                                        double frameRate = Double.parseDouble(settings.getProperty("frameRate"));
-                                        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("desktop");
-                                        grabber.setFormat("gdigrab");
-                                        grabber.setFrameRate(frameRate);
-                                        grabber.setImageWidth(screenWidth);
-                                        grabber.setImageHeight(screenHeight);
-                                        grabber.start();
-                                        utils.streaming.CanvasFrame frame = new utils.streaming.CanvasFrame("Screen Capture", utils.streaming.CanvasFrame.getDefaultGamma()/grabber.getGamma());
-                                        frame.setCanvasSize(screenWidth/2, screenHeight/2);
-                                        frame.setController(controller);
-                                        new Thread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            try {
-                                                while (frame.isVisible()) {
-                                                    System.out.println("dans le while");
-                                                    grabbedFrame = grabber.grab();
+                    double frameRate = Double.parseDouble(settings.getProperty("frameRate"));
+                    FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("desktop");
+                    grabber.setFormat("gdigrab");
+                    grabber.setFrameRate(frameRate);
+                    grabber.setImageWidth(screenWidth);
+                    grabber.setImageHeight(screenHeight);
+                    grabber.start();
+                    utils.streaming.CanvasFrame frame = new utils.streaming.CanvasFrame("Screen Capture", utils.streaming.CanvasFrame.getDefaultGamma()/grabber.getGamma());
+                    frame.setCanvasSize(screenWidth/2, screenHeight/2);
+                    frame.setController(controller);
+                    new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (frame.isVisible()) {
+                                System.out.println("dans le while");
+                                grabbedFrame = grabber.grab();
                                                     //Display frame in streamer window
                                                     //frame.showImage(grabbedFrame);
-                                                    controller.recorder(grabbedFrame);
-                                                }
-                                                frame.dispose();
-                                                controller.clean();
-                                                grabber.stop();
-                                                } catch (FrameGrabber.Exception ex) {
-                                                    Logger.getLogger(MenubarController.class.getName()).log(Level.SEVERE, null, ex);
-                                                } catch (FrameRecorder.Exception ex) {
-                                                    Logger.getLogger(MenubarController.class.getName()).log(Level.SEVERE, null, ex);
-                                                }
-                                            }
-                                        }).start();
-					break;
-				default:
-					System.err.println("Switch inconnu");
-					break;
-			}
-		}
-	}
+                                controller.recorder(grabbedFrame);
+                            }
+                            frame.dispose();
+                            controller.clean();
+                            grabber.stop();
+                        } catch (FrameGrabber.Exception ex) {
+                            Logger.getLogger(MenubarController.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (FrameRecorder.Exception ex) {
+                            Logger.getLogger(MenubarController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }}).start();
+                    break;
+		default:
+                    System.err.println("Switch inconnu");
+                    break;
+            }
+        }
+    }
 
-	public void aboutUs(ActionEvent event)
-	{
-            ResourceBundle bundle = ServiceFactory.getResourceBundle(false);
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle(bundle.getString("AboutUs_title"));
-            alert.setHeaderText(null);
-            alert.setContentText(bundle.getString("AboutUs_content"));
+    public void aboutUs(ActionEvent event)
+    {
+        ResourceBundle bundle = ServiceFactory.getResourceBundle(false);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(bundle.getString("AboutUs_title"));
+        alert.setHeaderText(null);
+        alert.setContentText(bundle.getString("AboutUs_content"));
 
-            ButtonType buttonTypeOne = new ButtonType(bundle.getString("AboutUs_website_btn"));
-            ButtonType buttonTypeCancel = new ButtonType(bundle.getString("AboutUs_close"), ButtonBar.ButtonData.CANCEL_CLOSE);
+        ButtonType buttonTypeOne = new ButtonType(bundle.getString("AboutUs_website_btn"));
+        ButtonType buttonTypeCancel = new ButtonType(bundle.getString("AboutUs_close"), ButtonBar.ButtonData.CANCEL_CLOSE);
 
-            alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);
 
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == buttonTypeOne) {
-                try {
-                    Desktop.getDesktop().browse(new URL(bundle.getString("AboutUs_URL")).toURI());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } 
-	}
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeOne) {
+            try {
+                Desktop.getDesktop().browse(new URL(bundle.getString("AboutUs_URL")).toURI());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } 
+    }
 }
